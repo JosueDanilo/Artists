aceb7df5df3cd3c40705f6393b46f3d7a58c8227
JosueDanilo <josueortiz@wposs.com>
2023-06-29 21:16:28 -0500

    IMPLEMENTACIÓN - Fragments principales
    * Se agregan los fragments principales y diseño basico.

---
+++ app/src/main/java/com/example/artists/view/ArtistDetailsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.artists.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.example.artists.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ArtistDetailsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ArtistDetailsFragment : Fragment(R.layout.fragment_artist_details) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/example/artists/view/TopArtistsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.artists.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.artists.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TopArtistsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TopArtistsFragment : Fragment(R.layout.fragment_top_artists) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
